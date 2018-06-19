1a6153c2f91ee0eeee67d1e5c940fe0e25608769
John Mark <dr3adlokk@gmail.com>
2018-06-19 15:00:23 -0700

    finally-burger-not-done

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var methodOverride = require('method-override')
+var morgan = require('morgan')
+var expresshbs = require('express-handlebars')
+
+var app = express()
+
+//midddleware
+// middleware
+app.use(morgan('dev'))
+app.engine('hbs', expresshbs({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyparser.urlencoded({
+    extended: true
+}))
+app.use(bodyparser.json())
+
+
+//routes
+var routes = require('./controllers/burgers_controller.js')
+app.use('/', routes);
+
+
+
+var routes = require('./controllers/burgers_controller.js')
+app.use('/', routes)
+
+var port = process.env.port || 5000
+app.listen(port)
+console.log('Served To ', + port)
